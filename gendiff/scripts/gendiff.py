b9a7b0f06d4bb8283bb73c642cfef51683e18fb9
krutovis <ilya.krutov.94@mail.ru>
2023-05-17 22:07:19 +0500

    added help for format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,9 +2,12 @@
 import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration'
                                  ' files and shows a difference.')
-parser.add_argument('-f', '--first_file', type=str, help='First file to compare') # noqa
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str, help='set format of output')
+# parser.add_argument('-f', '--first_file', type=str, help='First file to compare') # noqa
 
-parser.add_argument('-s', '--second_file', type=str, help='Second file to compare') # noqa
+# parser.add_argument('-s', '--second_file', type=str, help='Second file to compare') # noqa
 args = parser.parse_args()
 # print(args.first_file)
 
