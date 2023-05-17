336b7f2616ddec6732c8c4d6dc3030800fd3a4a2
krutovis <ilya.krutov.94@mail.ru>
2023-05-17 23:53:59 +0500

    added arguments to gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,46 @@
 #!/usr/bin/env python3
+import json
 import argparse
+
 parser = argparse.ArgumentParser(description='Compares two configuration'
                                  ' files and shows a difference.')
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
+parser.add_argument('first_file')
+parser.add_argument('second_file')
 parser.add_argument('-f', '--format', type=str, help='set format of output')
-# parser.add_argument('-f', '--first_file', type=str, help='First file to compare') # noqa
-
-# parser.add_argument('-s', '--second_file', type=str, help='Second file to compare') # noqa
 args = parser.parse_args()
-# print(args.first_file)
 
 
-def main():
-    return 'Hello'
+def generate_diff(first_file, second_file):
+    with open(first_file, 'r') as first_input:
+        with open(second_file, 'r') as second_input:
+            data1 = json.load(first_input)
+            print(data1)
+            data2 = json.load(second_input)
+            print(data2)
+    string = ''
+    return string
+
+
+'''
+Ваша библиотека также должна предоставлять модуль gendiff с функцией
+generate_diff().
+Вызов этой функции возвращает строку с разницей между содержимым двух файлов.
+
+Требования:
+
+Сравниваются данные, а не строки файлов
+Две строки дифа, которые отвечают за различия общего поля,
+должны находиться рядом.
+Причем сначала выводится строка, которая относится к первому файлу.
+Затем выводится строка, которая относится ко второму файлу
+(см. пример с timeout)
+Результатом работы функции generate_diff() является строка
+'''
+
+
+# def main(first_file, second_file):
+#     return 'Hello'
 
 
 if __name__ == '__main__':
-    main()
+    generate_diff(args.first_file, args.second_file)
